94297c55c0c904f0cb5150633d3671d2f7421342
Ryan Schmidt <ryanschmidt_3@hotmail.com>
2024-08-31 15:26:27 -0700

    add select previous mapping for cmp

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -7,6 +7,7 @@
       mapping = {
         "<CR>" = "cmp.mapping.confirm({ select = true })";
         "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+        "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
       };
 
       sources = [
