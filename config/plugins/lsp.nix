794eb816df26e84143b11e2285b6f379d87e264d
Ryan Schmidt <ryanschmidt_3@hotmail.com>
2024-08-31 16:47:26 -0700

    keymaps for lsp diagnostics

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -2,6 +2,21 @@
   plugins.lsp = {
     enable = true;
 
+    keymaps = {
+      diagnostic = {
+        "<leader>j" = "goto_next";
+        "<leader>k" = "goto_prev";
+      };
+
+      lspBuf = {
+        K = "hover";
+        gD = "references";
+        gd = "definition";
+        gi = "implementation";
+        gt = "type_definition";
+      };
+    };
+
     servers = {
       tsserver.enable = true;
 
