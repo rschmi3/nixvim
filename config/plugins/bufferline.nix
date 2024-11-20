669ab5a38251012b310c87d496b297b8a4c9c93e
Ryan Schmidt <ryanschmidt_3@hotmail.com>
2024-11-20 15:16:39 -0800

    add File Explorer tab to bufferline

--- config/plugins/bufferline.nix
+++ config/plugins/bufferline.nix
@@ -1,5 +1,13 @@
 {
   plugins.bufferline = {
     enable = true;
+    offsets = [
+      {
+        filetype = "NvimTree";
+        highlight = "Directory";
+        text = "File Explorer";
+        text_align = "center";
+      }
+    ];
   };
 }
