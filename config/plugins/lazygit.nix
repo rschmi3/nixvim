6571e475d8db6c1047af9bb5c8675538bea625c1
Ryan Schmidt <ryanschmidt_3@hotmail.com>
2025-01-06 11:43:21 -0800

    add autogroups and keybinds for lazygit

--- config/plugins/lazygit.nix
+++ config/plugins/lazygit.nix
@@ -1,5 +1,37 @@
 {
+  autoCmd = [
+    {
+      callback.__raw = ''
+        function()
+          vim.keymap.set("t", "<Esc><Esc>", "<Nop>", { buffer = true, silent = true })
+        end'';
+      desc = "Disable terminal mode exit in lazygit";
+      event = [ "TermOpen" ];
+      group = "lazygit";
+      pattern = "term://*lazygit*";
+    }
+  ];
+
+  autoGroups = {
+    lazygit = {
+      clear = true;
+    };
+  };
+
+  keymaps = [
+    {
+      action = "<cmd>LazyGit<cr>";
+      key = "<leader>lg";
+      mode = [ "n" ];
+      options = {
+        desc = "LazyGit";
+      };
+    }
+  ];
   plugins.lazygit = {
     enable = true;
+    settings = {
+      floating_window_use_plenary = 1;
+    };
   };
 }
