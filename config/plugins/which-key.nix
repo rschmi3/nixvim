4a02e492b8c79044db2a01176dbe5289cd36793a
Ryan Schmidt <ryanschmidt_3@hotmail.com>
2025-01-06 15:11:29 -0800

    set keybind for which key help

--- config/plugins/which-key.nix
+++ config/plugins/which-key.nix
@@ -1,4 +1,19 @@
 { pkgs, ... }:
 {
-  plugins.which-key.enable = true;
+  keymaps = [
+    {
+      action.__raw = ''
+        function()
+          require("which-key").show({ global = false })
+        end
+      '';
+      key = "<leader>?";
+      options = {
+        desc = "Buffer Local Keymaps (which-key)";
+      };
+    }
+  ];
+  plugins.which-key = {
+    enable = true;
+  };
 }
