051229d2668aa3e55e8d6e3fc822c1ba157d8579
Ryan Schmidt <ryanschmidt_3@hotmail.com>
2025-01-06 15:01:33 -0800

    update treesitter with disable function for big files

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -3,7 +3,18 @@
   plugins.treesitter = {
     enable = true;
     settings = {
-      highlight.enable = true;
+      highlight = {
+        enable = true;
+        disable.__raw = ''
+          function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end
+        '';
+      };
       indent.enable = true;
     };
   };
