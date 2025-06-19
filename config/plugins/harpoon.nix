7993f4282e1ea42582f1ad7570a8a6a7ee256896
Ryan Schmidt <ryanschmidt_3@hotmail.com>
2025-06-19 14:39:27 -0400

    change harpoon binds to use the home row instead of 1-5

--- config/plugins/harpoon.nix
+++ config/plugins/harpoon.nix
@@ -9,11 +9,11 @@
       vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
       vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-      vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-      vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-      vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-      vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
-      vim.keymap.set("n", "<C-5>", function() harpoon:list():select(5) end)
+      vim.keymap.set({"n", "v"}, "<C-h>", function() harpoon:list():select(1) end)
+      vim.keymap.set({"n", "v"}, "<C-j>", function() harpoon:list():select(2) end)
+      vim.keymap.set({"n", "v"}, "<C-k>", function() harpoon:list():select(3) end)
+      vim.keymap.set({"n", "v"}, "<C-l>", function() harpoon:list():select(4) end)
+      vim.keymap.set({"n", "v"}, "<C-;>", function() harpoon:list():select(5) end)
 
       -- Toggle previous & next buffers stored within Harpoon list
       vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
