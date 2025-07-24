785f32b98837d3eb237a81dcc50199eef512af05
Ryan Schmidt <ryanschmidt_3@hotmail.com>
2025-07-24 13:45:22 -0400

    remove 5th harpoon binding cause control ; doesn't work

--- config/plugins/harpoon.nix
+++ config/plugins/harpoon.nix
@@ -13,7 +13,6 @@
       vim.keymap.set({"n", "v"}, "<C-j>", function() harpoon:list():select(2) end, {desc = "Harpoon Buffer 2"})
       vim.keymap.set({"n", "v"}, "<C-k>", function() harpoon:list():select(3) end, {desc = "Harpoon Buffer 3"})
       vim.keymap.set({"n", "v"}, "<C-l>", function() harpoon:list():select(4) end, {desc = "Harpoon Buffer 4"})
-      vim.keymap.set({"n", "v"}, "<C-;>", function() harpoon:list():select(5) end, {desc = "Harpoon Buffer 5"})
 
       -- Toggle previous & next buffers stored within Harpoon list
       vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end, {desc = "Harpoon Previous Buffer"})
