87fc8a1b3d0fcdfc00faf18abf0e53a7620d5748
Ryan Schmidt <ryanschmidt_3@hotmail.com>
2025-12-19 19:09:06 -0500

    remove osc clipboard stuff

--- config/options.nix
+++ config/options.nix
@@ -20,19 +20,19 @@
       whichwrap = "b,s,<,>,h,l,[,]";
     };
 
-    # This will use OSC 52 when available, fall back to system clipboard otherwise
-    extraConfigLua = ''
-      vim.g.clipboard = {
-        name = "OSC 52",
-        copy = {
-          ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-          ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-        },
-        paste = {
-          ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-          ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-        },
-      }
-    '';
+    #   # This will use OSC 52 when available, fall back to system clipboard otherwise
+    #   extraConfigLua = ''
+    #     vim.g.clipboard = {
+    #       name = "OSC 52",
+    #       copy = {
+    #         ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    #         ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    #       },
+    #       paste = {
+    #         ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    #         ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    #       },
+    #     }
+    #   '';
   };
 }
